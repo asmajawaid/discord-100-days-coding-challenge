8cd75e97f8f852597c00300524d40572e02a9167
asmajawaid <arshadasma45678@gmail.com>
2024-05-13 20:41:26 +0500

    update

--- day#23/Q69/main.ts
+++ day#23/Q69/main.ts
@@ -15,15 +15,3 @@ console.log(devideAndReminder(22, 5));// Shows { quotient: 4, remainder: 2 }
 
 
 
-function divideAndRemainder(
-    dividend: number,
-    divisor: number
-  ): { quotient: number; remainder: number } {
-    // Calculates the quotient and remainder
-    let quotient = Math.floor(dividend / divisor);
-    let remainder = dividend % divisor;
-    return { quotient, remainder };
-  }
-  
-  // Trying it with 10 divided by 3
-  console.log(divideAndRemainder(10, 3)); 
